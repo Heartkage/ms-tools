1afe0ce7482a7b9f7b3c930b8664d59affe70127
Heartkage <p124leo@gmail.com>
2025-05-10 00:15:56 +0800

    fix: remove /ms-tools for changing domain

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,9 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig = {
   output: 'export', // Enable static exports for GitHub Pages
   // Always use basePath in production
-  basePath: process.env.NODE_ENV === 'production' ? '/ms-tools' : '',
+  // basePath: process.env.NODE_ENV === 'production' ? '/ms-tools' : '',
   // Configure asset prefix for production
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/ms-tools' : '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/ms-tools' : '',
   images: {
     unoptimized: true
   },
