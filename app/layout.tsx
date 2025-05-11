855ad60ec10bf096ef5a462f4c44b978adae90b4
Heartkage <p124leo@gmail.com>
2025-05-11 11:28:01 +0800

    add: set favicon for the page

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,12 @@ export const dynamic = 'force-static'
 export const metadata: Metadata = {
   title: 'MapleStory Tool',
   description: 'A collection of tools for MapleStory',
+  icons: {
+    icon: [
+      { url: '/favicon.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-192.png', sizes: '192x192', type: 'image/png' }
+    ]
+  },
 }
 
 export default function RootLayout({
@@ -19,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={notoSansTC.variable}>
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="32x32" type="image/png" />
+        <link rel="icon" href="/favicon-192.png" sizes="192x192" type="image/png" />
+      </head>
       <body className={`${notoSansTC.className} antialiased`}>
         <Providers>
           <AnalyticsProvider>
